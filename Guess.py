404ac33686414ae691a51a09fc3ad6b7d6fe924c
Atharv-deatheaters <62825797+Atharv-deatheaters@users.noreply.github.com>
2020-11-06 13:13:30 +0530

    Add files via upload

---
+++ Guess.py
@@ -0,0 +1,23 @@
+
+number = input("guess a number between 1 to 9 :")
+
+if (number == "1"):
+    print("your gues was very low try again")
+elif(number == "2"):
+    print("your guess was very low try again")
+elif(number == "3"):
+    print("your guess was very low try again")
+elif(number == "4"):
+    print("your guess is near try again to be perfect")
+elif(number == "5"):
+    print("your guess is pretty accurate try again")
+elif(number == "6"):
+    print("your have got it correct nice unless until you have tried all of the numbers")
+elif(number == "7"):
+    print("your number is pretty accurate but not correct try again")
+elif(number == "8"):
+    print("your number is very high pls try again ")
+elif(number == "9"):
+    print("this is the latest number.So this cant be the answer use your brain")
+else:
+    print("this is out of the guess and not an alphabet")
